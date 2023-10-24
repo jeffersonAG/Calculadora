3db8cec5d0df0df142b03d8234c28c3ccf58d459
Kasyrgan04 <dyguerrero@estudiantec.cr>
2023-10-23 18:57:06 -0600

    Compuertas

--- CALCULADORA/src/main/java/com/example/calculadora/Interfaz.java
+++ CALCULADORA/src/main/java/com/example/calculadora/Interfaz.java
@@ -1,12 +1,16 @@
 package com.example.calculadora;
 
 import com.example.calculadora.Arbol.ArbolBinario;
+import com.example.calculadora.Arbol.ArbolCompuertas;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -15,10 +19,12 @@ public class Interfaz extends Application {
 
     private TextField textField;
     private ArbolBinario arbol;
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) {
-        arbol = new ArbolBinario();
+        this.primaryStage=primaryStage;
+    	arbol = new ArbolBinario();
 
         textField = new TextField();
         textField.setPromptText("0");
@@ -36,7 +42,7 @@ public class Interfaz extends Application {
                 "7", "8", "9", "*",
                 "4", "5", "6", "-",
                 "1", "2", "3", "+",
-                "0", "←", "="
+                "0", "←", "=", "BIN"
         };
 
         int row = 0;
@@ -44,11 +50,17 @@ public class Interfaz extends Application {
 
         for (String tecla : teclas) {
             Button boton = new Button(tecla);
+            if(tecla.equals("BIN")) {
+            	boton.setMinSize(50, 50);
+                boton.setFont(Font.font(18));
+                boton.setOnAction(e -> irACompuertas());
+                botones.add(boton, col, row);
+            } else{
             boton.setMinSize(50, 50);
             boton.setFont(Font.font(18));
             boton.setOnAction(e -> procesarTecla(tecla));
             botones.add(boton, col, row);
-
+            }
             col++;
             if (col == 4) {
                 col = 0;
@@ -88,8 +100,108 @@ public class Interfaz extends Application {
             if (!textoActual.isEmpty()) {
                 textField.setText(textoActual.substring(0, textoActual.length() - 1));
             }
-        } else {
+        }else {
             textField.appendText(tecla);
-        }
+        } 
+    }
+    
+    private void irACompuertas() {
+    	Compuertas segundaVentana=new Compuertas(primaryStage);
+    	segundaVentana.show();
+    }
+    
+    public class Compuertas extends Stage{
+    	private ArbolCompuertas tree;
+    	
+    	public Compuertas(Stage Interfaz) {
+    		tree=new ArbolCompuertas();
+    		VBox root1 = new VBox(10);
+    		root1.setAlignment(Pos.CENTER);
+    		textField = new TextField();
+            textField.setPromptText("0");
+            textField.setFont(Font.font(18));
+            textField.setEditable(false);
+    		
+            GridPane botones = new GridPane();
+            botones.setAlignment(Pos.CENTER);
+            botones.setHgap(10);
+            botones.setVgap(10);
+
+            // Crear botones numéricos, de operadores, paréntesis, Borrar y "="
+            String[] teclas = {
+                    "C", "(", ")", "AND",
+                    "7", "8", "9", "OR",
+                    "4", "5", "6", "XOR",
+                    "1", "2", "3", "NOT",
+                    "0", "←", "=", "DEC"
+            };
+
+            int row = 0;
+            int col = 0;
+
+            for (String tecla : teclas) {
+                Button boton = new Button(tecla);
+                if(tecla.equals("DEC")) {
+                	boton.setMinSize(50, 50);
+                    boton.setFont(Font.font(18));
+                    boton.setOnAction(e -> {
+                    	this.close();
+                    	Interfaz.show();
+                    });
+                    botones.add(boton, col, row);
+                } else{
+	                boton.setMinSize(50, 50);
+	                boton.setFont(Font.font(18));
+	                boton.setOnAction(e -> procesar(tecla));
+	                botones.add(boton, col, row);
+                }
+                col++;
+                if (col == 4) {
+                    col = 0;
+                    row++;
+                }
+                if(tecla.equals("9") || tecla.equals("8") || tecla.equals("7") ||
+                   tecla.equals("6") || tecla.equals("5") || tecla.equals("4") ||
+                   tecla.equals("3") || tecla.equals("2")) {
+                	boton.setDisable(true);
+                }
+                
+            }
+            root1.getChildren().addAll(textField,botones);
+    		StackPane root= new StackPane();
+    		root.getChildren().addAll(root1);
+    		
+    		Scene scene=new Scene(root,300,350);
+    		
+    		this.setTitle("Calculadora");
+    		this.setScene(scene);
+    		
+    		
+    	}
+    	private void procesar(String tecla) {
+            if (tecla.equals("C")) {
+                textField.setText("");
+            } else if (tecla.equals("=")) {
+                String expresion = textField.getText();
+                tree.construirArbol(expresion);
+                try {
+                    boolean resultado = tree.evaluar();
+                    textField.setText(resultado ? "1" : "0");
+                } catch (Exception e) {
+                	e.printStackTrace();
+                    textField.setText("Error" + e.getMessage());
+                }
+            } else if (tecla.equals("←")) { // Botón de Borrar
+                String textoActual = textField.getText();
+                if (!textoActual.isEmpty()) {
+                    textField.setText(textoActual.substring(0, textoActual.length() - 1));
+                }
+            }else {
+                textField.appendText(tecla);
+            }
+    	}
     }
 }
+
+
+
--- CALCULADORA/src/main/java/module-info.java
+++ CALCULADORA/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.calculadora {
     requires javafx.controls;
     requires javafx.fxml;
     requires opencv;
+	requires javafx.graphics;
 
 
     opens com.example.calculadora to javafx.fxml;
