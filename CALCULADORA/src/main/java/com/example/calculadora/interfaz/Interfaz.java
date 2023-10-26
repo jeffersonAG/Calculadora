8f3774e6a79f8442ee5f0c372622e51899c6f87c
jeffersonAG <gutierrez22754@gmail.com>
2023-10-26 06:54:58 -0600

    servidor conectado
    servidor conectado ala interfaz por medio de un botón llamado (server)

--- CALCULADORA/src/main/java/com/example/calculadora/interfaz/Interfaz.java
+++ CALCULADORA/src/main/java/com/example/calculadora/interfaz/Interfaz.java
@@ -1,7 +1,14 @@
 package com.example.calculadora.interfaz;
 
+/**
+ * Clase que representa la interfaz de una calculadora de expresiones matemáticas
+ * utilizando un árbol binario para evaluar las expresiones.
+ */
+
 import com.example.calculadora.Arbol.ArbolBinario;
 import com.example.calculadora.Arbol.ArbolCompuertas;
+import com.example.calculadora.Servidor.servidor;
+import com.example.calculadora.reconocimiento_de_patrones.ReconocimientoFacial;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -17,10 +24,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-/**
- * Clase que representa la interfaz de una calculadora de expresiones matemáticas
- * utilizando un árbol binario para evaluar las expresiones.
- */
 public class Interfaz extends Application {
 
     private TextField textField;
@@ -29,8 +32,8 @@ public class Interfaz extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage=primaryStage;
-    	arbol = new ArbolBinario();
+        this.primaryStage = primaryStage;
+        arbol = new ArbolBinario();
 
         textField = new TextField();
         textField.setPromptText("0");
@@ -47,7 +50,7 @@ public class Interfaz extends Application {
                 "7", "8", "9", "*",
                 "4", "5", "6", "-",
                 "1", "2", "3", "+",
-                "0", "←", "=", "^", "CAM","M.log","BIN" // Agregar el botón "CAM"
+                "0", "←", "=", "^", "CAM", "Server", "BIN"
         };
 
         int row = 0;
@@ -55,16 +58,26 @@ public class Interfaz extends Application {
 
         for (String tecla : teclas) {
             Button boton = new Button(tecla);
-            if(tecla.equals("BIN")) {
-            	boton.setMinSize(50, 50);
+            if (tecla.equals("BIN")) {
+                boton.setMinSize(50, 50);
                 boton.setFont(Font.font(18));
                 boton.setOnAction(e -> irACompuertas());
                 botones.add(boton, col, row);
-            } else{
-            boton.setMinSize(50, 50);
-            boton.setFont(Font.font(18));
-            boton.setOnAction(e -> procesarTecla(tecla));
-            botones.add(boton, col, row);
+            } else if (tecla.equals("CAM")) {
+                boton.setMinSize(50, 50);
+                boton.setFont(Font.font(18));
+                boton.setOnAction(e -> abrirReconocimientoFacial());
+                botones.add(boton, col, row);
+            } else if (tecla.equals("Server")) {
+                boton.setMinSize(50, 50);
+                boton.setFont(Font.font(18));
+                boton.setOnAction(e -> iniciarServidor());
+                botones.add(boton, col, row);
+            } else {
+                boton.setMinSize(50, 50);
+                boton.setFont(Font.font(18));
+                boton.setOnAction(e -> procesarTecla(tecla));
+                botones.add(boton, col, row);
             }
             col++;
             if (col == 4) {
@@ -84,6 +97,13 @@ public class Interfaz extends Application {
         primaryStage.show();
     }
 
+    private void abrirReconocimientoFacial() {
+        // Código para abrir la ventana de reconocimiento facial
+        ReconocimientoFacial reconocimientoFacialApp = new ReconocimientoFacial();
+        Stage reconocimientoStage = new Stage();
+        reconocimientoFacialApp.start(reconocimientoStage);
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -96,7 +116,6 @@ public class Interfaz extends Application {
 
             // Establece una conexión con el servidor
             try (Socket socket = new Socket("localhost", 12345)) {
-                // Crea flujos de entrada y salida
                 ObjectOutputStream salida = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream entrada = new ObjectInputStream(socket.getInputStream());
 
@@ -120,24 +139,32 @@ public class Interfaz extends Application {
         }
     }
 
-
     private void irACompuertas() {
         Stage stage = (Stage) textField.getScene().getWindow();
         stage.close();
 
-    	Compuertas segundaVentana=new Compuertas(primaryStage);
-    	segundaVentana.show();
+        Compuertas segundaVentana = new Compuertas(primaryStage);
+        segundaVentana.show();
     }
 
-    public class Compuertas extends Stage{
-    	private ArbolCompuertas tree;
+    private void iniciarServidor() {
+        // Código para iniciar el servidor
+        Thread serverThread = new Thread(() -> {
+            servidor.main(null);
+        });
+        serverThread.setDaemon(true);
+        serverThread.start();
+    }
 
-    	public Compuertas(Stage Interfaz) {
-    		tree=new ArbolCompuertas();
-    		VBox root1 = new VBox(10);
-    		root1.setAlignment(Pos.CENTER);
+    public class Compuertas extends Stage {
+        private ArbolCompuertas tree;
+
+        public Compuertas(Stage Interfaz) {
+            tree = new ArbolCompuertas();
+            VBox root1 = new VBox(10);
+            root1.setAlignment(Pos.CENTER);
             root1.setStyle("-fx-background-color: black;");
-    		textField = new TextField();
+            textField = new TextField();
             textField.setPromptText("0");
             textField.setFont(Font.font(18));
             textField.setEditable(false);
@@ -147,13 +174,12 @@ public class Interfaz extends Application {
             botones.setHgap(10);
             botones.setVgap(10);
 
-            // Crear botones numéricos, de operadores, paréntesis, Borrar y "="
             String[] teclas = {
                     "C", "(", ")", "AND",
                     "7", "8", "9", "OR",
                     "4", "5", "6", "XOR",
                     "1", "2", "3", "NOT",
-                    "0", "←", "=", "DEC", "CAM","M.log",
+                    "0", "←", "=", "DEC", "CAM", "M.log",
             };
 
             int row = 0;
@@ -161,48 +187,45 @@ public class Interfaz extends Application {
 
             for (String tecla : teclas) {
                 Button boton = new Button(tecla);
-                if(tecla.equals("DEC")) {
-                	boton.setMinSize(50, 50);
+                if (tecla.equals("DEC")) {
+                    boton.setMinSize(50, 50);
                     boton.setFont(Font.font(18));
                     boton.setOnAction(e -> {
-                    	this.close();
+                        this.close();
 
-                        // Crear una nueva instancia de la clase Interfaz
                         Interfaz interfaz = new Interfaz();
                         Stage newStage = new Stage();
                         interfaz.start(newStage);
                     });
                     botones.add(boton, col, row);
-                } else{
-	                boton.setMinSize(50, 50);
-	                boton.setFont(Font.font(18));
-	                boton.setOnAction(e -> procesar(tecla));
-	                botones.add(boton, col, row);
+                } else {
+                    boton.setMinSize(50, 50);
+                    boton.setFont(Font.font(18));
+                    boton.setOnAction(e -> procesar(tecla));
+                    botones.add(boton, col, row);
                 }
                 col++;
                 if (col == 4) {
                     col = 0;
                     row++;
                 }
-                if(tecla.equals("9") || tecla.equals("8") || tecla.equals("7") ||
-                   tecla.equals("6") || tecla.equals("5") || tecla.equals("4") ||
-                   tecla.equals("3") || tecla.equals("2")) {
-                	boton.setDisable(true);
+                if (tecla.equals("9") || tecla.equals("8") || tecla.equals("7") ||
+                        tecla.equals("6") || tecla.equals("5") || tecla.equals("4") ||
+                        tecla.equals("3") || tecla.equals("2")) {
+                    boton.setDisable(true);
                 }
-
             }
-            root1.getChildren().addAll(textField,botones);
-    		StackPane root= new StackPane();
-    		root.getChildren().addAll(root1);
+            root1.getChildren().addAll(textField, botones);
+            StackPane root = new StackPane();
+            root.getChildren().addAll(root1);
 
-    		Scene scene=new Scene(root,350,400);
+            Scene scene = new Scene(root, 350, 400);
 
-    		this.setTitle("Calculadora");
-    		this.setScene(scene);
+            this.setTitle("Calculadora");
+            this.setScene(scene);
+        }
 
-
-    	}
-    	private void procesar(String tecla) {
+        private void procesar(String tecla) {
             if (tecla.equals("C")) {
                 textField.setText("");
             } else if (tecla.equals("=")) {
@@ -212,20 +235,17 @@ public class Interfaz extends Application {
                     boolean resultado = tree.evaluar();
                     textField.setText(resultado ? "1" : "0");
                 } catch (Exception e) {
-                	e.printStackTrace();
+                    e.printStackTrace();
                     textField.setText("Error" + e.getMessage());
                 }
-            } else if (tecla.equals("←")) { // Botón de Borrar
+            } else if (tecla.equals("←")) {
                 String textoActual = textField.getText();
                 if (!textoActual.isEmpty()) {
                     textField.setText(textoActual.substring(0, textoActual.length() - 1));
                 }
-            }else {
+            } else {
                 textField.appendText(tecla);
             }
-    	}
+        }
     }
 }
-
-
-
